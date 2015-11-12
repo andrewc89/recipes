d1b07ce8698d070677b2909479459393895acf1c
Andrew Curcie <andrew.curcie@gmail.com>
2015-11-11 20:17:41 -0500

    set up livereload, watch

--- public/js/components/recipes.js
+++ public/js/components/recipes.js
@@ -1,8 +1,26 @@
 var React = require("react");
+var request = require("superagent");
 
 var Recipes = React.createClass({
+  getInitialState: function () {
+    return {
+      ingredients: []
+    };
+  },
+  componentDidMount: function () {
+    var self = this;
+    request.get("/ingredients")
+      .end(function (err, res) {
+        self.setState({ ingredients: res.body });
+      });
+  },
+
   render: function () {
-    return <div>Hello World!</div>;
+    console.log("render");
+    return (
+      <div>
+      </div>
+    );
   }
 });
 
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require("express");
-var ingredients = ["Nelson Sauvin", "Cascade", "Simcoe", "Crystal Malt", "Pale Malt"];
-
+var livereload = require("livereload");
+var ingredients = [
+  { id: "0", name: "Nelson Sauvin" },
+  { id: "0", name: "Cascade" },
+  { id: "0", name: "Simcoe" },
+  { id: "0", name: "Crystal Malt" },
+  { id: "0", name: "Pale Malt" }
+];
 
 module.exports = {
   start: function() {
+    livereload.createServer().watch("public/dev.js");
     var app = express();
     app.use(express.static("public"));
     app.get("/ingredients", function (req, res) {
